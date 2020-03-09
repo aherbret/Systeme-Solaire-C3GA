513f2904b3a9feb1f87cd76d96be6dabc95ad93e
Amel BEN HAMOUDA <amel@debian>
2020-03-09 16:29:11 +0100

    rien

--- src/space/main.cpp
+++ src/space/main.cpp
@@ -165,6 +165,7 @@ int main(int argc, char** argv) {
     float boost_speed = 0.5;
     // Application loop:
     bool done = false;
+
     while (!done) {
         //glm::mat4 ViewMatrix = Camera.getViewMatrix();
         // Event loop:
@@ -247,10 +248,27 @@ int main(int argc, char** argv) {
         glDrawArrays(GL_TRIANGLES, 0, sphere.getVertexCount());
         glActiveTexture(GL_TEXTURE0);
         tex.activeAndBindTexture(GL_TEXTURE0, 0); // la texture earthTexture est bindée sur l'unité GL_TEXTURE0
-        tex.activeAndBindTexture(GL_TEXTURE1, 0); // la texture cloudTexture est bindée sur l'unité GL_TEXTURE1
         glUniform1i(sunProgram.uTexture, 0);
 
         // Soleil (grande) avec C3GA
+        moonProgram.m_Program.use();
+        glUniform1i(moonProgram.uTexture, 0);
+
+        glm::mat4 MVMatrix = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5)); // Translation
+        glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+        MVMatrix = glm::rotate(MVMatrix, windowManager.getTime(), AxesRotation.at(1)); // Translation * Rotation
+        MVMatrix = glm::translate(MVMatrix, Translations.at(1)); // Translation * Rotation * Translation
+        MVMatrix = glm::scale(MVMatrix, glm::vec3(0.2, 0.2, 0.2)); // Translation * Rotation * Translation * Scale
+       
+        glUniformMatrix4fv(moonProgram.uMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+        glUniformMatrix4fv(moonProgram.uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+        NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+        glUniformMatrix4fv(moonProgram.uNormalMatrix, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
+		tex.activeAndBindTexture(GL_TEXTURE0, texture[1]);
+        glDrawArrays(GL_TRIANGLES, 0, sphere.getVertexCount());
+        glActiveTexture(GL_TEXTURE0);
+        tex.activeAndBindTexture(GL_TEXTURE0, 0); // la texture earthTexture est bindée sur l'unité GL_TEXTURE2
+        glUniform1i(moonProgram.uTexture, 0);
 
 
         glBindVertexArray(0);
